0b3fab2984c98e317f55c5848125b097098a7e99
Ostap <Ostapkriven13@gmail.com>
2023-06-19 19:13:35 +0300

    і
    і

--- js/function.js
+++ js/function.js
@@ -11,3 +11,20 @@ class Switch {
 		this.removeAttribute("data-pristine");
 	}
 }
+
+
+
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const carousel = document.querySelector('.carousel');
+
+prevBtn.addEventListener('click', () => {
+  carousel.style.animationPlayState = 'paused';
+  carousel.style.animationDirection = 'reverse';
+});
+
+nextBtn.addEventListener('click', () => {
+  carousel.style.animationPlayState = 'paused';
+  carousel.style.animationDirection = 'normal';
+});
